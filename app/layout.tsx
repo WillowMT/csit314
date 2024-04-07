1ce70affb25b319dd4f22bc677e9afad0769afc4
WillowMT <waiyanmt.20@icloud.com>
2024-04-08 01:24:59 +0800

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navigation from "@/components/nav";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Navigation />
           {children}
           <SpeedInsights />
+          <Analytics />
       </body>
     </html>
   );
