e751ca702bf549cc6ad86088f3df50562bd66322
Min Khant Thu <minkhantthu213@gmail.com>
2024-05-10 16:15:37 +0800

    frontend changes for buy and buy_1 pages

--- app/buy/collapse.tsx
+++ app/buy/collapse.tsx
@@ -1,122 +1,69 @@
 'use client'
 import React from "react";
-import {AccordionItem} from "@nextui-org/react";
-import {Input} from "@nextui-org/react";
+import {AccordionItem, Input, Button, RadioGroup, Radio} from "@nextui-org/react";
 import {SearchIcon} from "./SearchIcon";
-import {Button} from "@nextui-org/react";
-import {RadioGroup, Radio} from "@nextui-org/react";
 import {MyAccordion} from "./MyAccordion";
 import './styles.css';
 
 export default function Collapse() {
+    const [selected, setSelected] = React.useState("onsale");
 
     return (
-        <div className="flex w-full flex-col">
+        <div>
             <div className="filter_text">
                 Property Filter
             </div>
             <MyAccordion variant="light" color="gray" defaultExpandedKeys={["1"]} isCompact>
             <AccordionItem key="1" aria-label="Filter" title="">
-            <div className = "filter_bar">
-            <div className="search_bar">
-                <Input
-                    label="Search"
-                    isClearable
-                    radius="lg"
-                    classNames={{
-                    label: "text-black/50 dark:text-white/90",
-                    input: [
-                        "bg-transparent",
-                        "text-black/90 dark:text-white/90",
-                        "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-                    ],
-                    innerWrapper: "bg-transparent",
-                    inputWrapper: [
-                        "shadow-xl",
-                        "bg-default-200/50",
-                        "dark:bg-default/60",
-                        "backdrop-blur-xl",
-                        "backdrop-saturate-200",
-                        "hover:bg-default-200/70",
-                        "dark:hover:bg-default/70",
-                        "group-data-[focused=true]:bg-default-200/50",
-                        "dark:group-data-[focused=true]:bg-default/60",
-                        "!cursor-text",
-                    ],
-                    }}
-                    placeholder="Type to search..."
-                    startContent={
-                    <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
-                    }
-                />
+            <div className="flex flex-col items-center">
+                <div className="w-[500px] h-[100px] pl-6 rounded-2xl flex justify-center items-center">
+                    <Input
+                        className=""
+                        label="Search by location"
+                        isClearable
+                        radius="lg"
+                        classNames={{
+                        label: "text-black/50 dark:text-white/90",
+                        input: [
+                            "text-black/90 dark:text-white/90",
+                            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                        ],
+                        innerWrapper: "bg-transparent",
+                        inputWrapper: [
+                            "shadow-xl",
+                            "bg-default-50",
+                            "dark:bg-default/60",
+                            "backdrop-blur-xl",
+                            "backdrop-saturate-200",
+                            "hover:bg-default-900/70",
+                            "dark:hover:bg-default/70",
+                            "group-data-[focused=true]:bg-default-200/50",
+                            "dark:group-data-[focused=true]:bg-default/60",
+                            "!cursor-text",
+                        ],
+                        }}
+                        placeholder="Type to search..."
+                        startContent={
+                        <SearchIcon className="text-black/50 mb-0.5 dark:text-black/90 text-slate-400 pointer-events-none flex-shrink-0" />
+                        }
+                    />
+                </div>
+                <div className="">     
+                    <RadioGroup orientation="horizontal"
+                    color="default"
+                    value={selected}
+                    onValueChange={setSelected}
+                    defaultValue="onsale">
+                    <Radio value="onsale">On Sale</Radio>
+                    <Radio value="sold">Sold</Radio>
+                    </RadioGroup>
+                </div>
             </div>
-            <div className="min_price">
-                Price
-                <Input
-                type="number"
-                placeholder="0.00"
-                startContent={
-                    <div className="pointer-events-none flex items-center">
-                    <span className="text-default-400 text-small">$</span>
-                    </div>
-                }
-                />
+            <div className="flex py-4 justify-end pr-5">
+                    <Button>
+                        Apply filter
+                    </Button>
             </div>
-            <div className="max_price">
-                <Input
-                type="number"
-                placeholder="0.00"
-                startContent={
-                    <div className="pointer-events-none flex items-center">
-                    <span className="text-default-400 text-small">$</span>
-                    </div>
-                }
-                />
-            </div>
-            <div className="bedroom_no">
-                Bedrooms       
-                <RadioGroup orientation="horizontal"
-                color="default"
-                defaultValue="any_bedroom">
-                <Radio value="any_bedroom">Any</Radio>
-                <Radio value="1_bedroom">1</Radio>
-                <Radio value="2_bedroom">2</Radio>
-                <Radio value="3_bedroom">3</Radio>
-                <Radio value="4_bedroom">4</Radio>
-                <Radio value="5_bedroom">5</Radio>
-                </RadioGroup>
-            </div>
-            <div className="bathroom_no">
-                Bathrooms
-                <RadioGroup orientation="horizontal"
-                color="default"
-                defaultValue="any_bathroom">
-                <Radio value="any_bathroom">Any</Radio>
-                <Radio value="1_bathroom">1</Radio>
-                <Radio value="2_bathroom">2</Radio>
-                <Radio value="3_bathroom">3</Radio>
-                <Radio value="4_bathroom">4</Radio>
-                <Radio value="5_bathroom">5</Radio>
-                </RadioGroup>
-            </div>
-            <div className="property_type_filter">
-                Property Type
-                <RadioGroup orientation="horizontal"
-                color="default"
-                defaultValue="any_type">
-                <Radio value="any_type">Any</Radio>
-                <Radio value="HDB">HDB</Radio>
-                <Radio value="Land">Land</Radio>
-                <Radio value="Condominium">Condominium</Radio>
-                <Radio value="Apartment">Apartment</Radio>
-                </RadioGroup>
-            </div>
-            <div>
-                <Button className="search_button">
-                    Apply filter
-                </Button>
-            </div>
-        </div>
             </AccordionItem>
             </MyAccordion>
         </div>
--- app/buy_1/page.tsx
+++ app/buy_1/page.tsx
@@ -2,8 +2,8 @@ import Navigation from "@/components/nav";
 import './style.css';
 import React from "react";
 import AgentCard from './agentCard';
-import ShortList from "./shortlistIcon";
-import {Divider, Chip} from "@nextui-org/react";
+import {Divider, Chip, Checkbox} from "@nextui-org/react";
+import {PlusIcon} from './PlusIcon.jsx';
 
 const YourPage = () => {
 
@@ -21,16 +21,14 @@ const YourPage = () => {
                     <div className="image"><img src='https://www.bankrate.com/2023/06/12125257/buying-a-house-worth-it.jpg?auto=webp&optimize=high&crop=16:9&width=912' alt="image 5" /></div>
                 </div>
             </div>
-                <div className="info_box">
-                    <div className="max-w-md">
+                <div className=" flex justify-center">
+                    <div className="w-[1300px]">
                         <div className="space-y-1">
-                            <br></br>
-                            <br></br>
-                            <h4 className="text-3xl font-bold">Property 1</h4>
-                            <p className="text-medium">123 Figuenrter Street, Gedasg, 1235</p>
+                            <h4 className="text-3xl font-bold pl-4">Property 1</h4>
+                            <p className="text-large pl-4">123 Figuenrter Street, Gedasg, 1235</p>
                         </div>
                         <br></br>
-                        <div className="flex h-5 items-center space-x-4 text-small">
+                        <div className="flex h-2 items-center space-x-4 text-small justify-start pl-4">
                             <Chip color="warning" variant="solid">HDB</Chip>
                             <Divider orientation="vertical" />
                             <Chip color="warning" variant="solid">99 years lease</Chip>
@@ -38,33 +36,35 @@ const YourPage = () => {
                             <Chip color="warning" variant="solid">Built in 2022</Chip>
                         </div>
                     <Divider className="my-5" />
-                        <div className="flex h-5 items-center space-x-10 text-small">
-                            <div>Starting from</div>
+                        <div className="flex h-5 items-center space-x-10 justify-start pl-4">
+                            <div className=" text-medium">Starting from</div>
                             <Divider orientation="vertical" />
                             <div className=" text-xl">$120,000,000</div>
                         </div>
                     <Divider className="my-5" />
-                        <div className="flex h-5 items-center space-x-4 text-small">
+                        <div className="flex h-5 items-center space-x-4 text-small justify-start pl-4">
                             <img className="bed_icon" src="https://icons.veryicon.com/png/o/miscellaneous/alan-ui/ios-bed-3.png" alt="bed" />
                             
                             <div>2</div>
                             <Divider orientation="vertical" />
                             <img className="bed_icon" src="https://icons.veryicon.com/png/o/miscellaneous/site-icon-library-1/bathroom-products-1.png" alt="bed" />
                             
-                            <div>2</div>
+                            <div>3</div>
                             <Divider orientation="vertical" />
                             <img className="bed_icon" src="https://icons.veryicon.com/png/o/construction-tools/supermap-gis-product-dark-color-function-icon/measured-area.png" alt="bed" />
                             
-                            <div>2</div>
+                            <div>200 sq/ft</div>
                         </div>
-                    <Divider className="my-5" />
-                        <div className="flex h-5 items-center space-x-4 text-small">
-                            <ShortList></ShortList>
+                        <Divider className="my-5" />
+                            <p className="text-medium pb-2 pl-4">Contact Agent</p>
+                            <div className="pl-4">
+                                <AgentCard></AgentCard>
+                            </div>
+                        <Divider className="my-5" />
+                        <div className="pl-4">
+                            <Checkbox icon={<PlusIcon size={undefined} height={undefined} width={undefined}></PlusIcon>} color="warning">Add to shortlist</Checkbox>
                         </div>
                     </div>
-                </div>
-                <div className="agent_card">
-                    <AgentCard></AgentCard>
                 </div>
         </div>
     )
