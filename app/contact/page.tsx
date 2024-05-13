80f17bda926bb8ee5fbdf0c193e8d75c9f7784fa
HanShin0512 <166029419+HanShin0512@users.noreply.github.com>
2024-05-13 23:20:22 +0800

    make about us page responsive, code contact us page, remove svg from nav bar and replace buy property with website logo

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,29 @@
 import Navigation from "@/components/nav";
-
+import './contact-style.css'
 export default function Page() {
     return(
         <div>
+            <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
             <Navigation />
-            contact page
+            <section className="contact">
+                <p className="title">Contact Us</p>
+                <div className="box">
+                    <div className="left">
+                        <img src="https://images.pexels.com/photos/5331968/pexels-photo-5331968.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+                    </div>
+                    <div className="right">
+                        <div className="icons">
+                            <p class='bx bxl-facebook'></p>
+                            <p class='bx bxl-instagram' ></p>
+                            <p class='bx bxl-twitter' ></p>
+                        </div>
+                        <p>eliteestate@gmail.com</p>
+                        <p>https://www.facebook.com/EliteEstate</p>
+                        <p>https://www.instagram.com/elite_estate</p>
+                        <p>https://twitter.com/EliteEstateHQ</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -10,14 +10,12 @@ export default async function Navigation() {
         <div className=" bg-brand-200">
             <nav className=" flex p-4">
                 <div className=" grid place-content-center mr-auto sm:mr-0">
-                    <Link href={"/"}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                        </svg>
-                    </Link>
                 </div>
                 <div className="flex list-none space-x-10 mr-auto ml-8 text-sm underline">
-                    <Link className="grid place-items-center hover:underline" href={"/buy"}>Buy Property</Link>
+                <Link className="grid place-items-center hover:underline" href={"/buy"} style={{ width: "100px", height: "30px" }}>
+                    <img src="/logo-nav.png" />
+                </Link>
+
                     {
                         session.role === "SELLER" && (
                             <Link className="grid place-items-center hover:underline" href={"/sell"}>Sell</Link>
